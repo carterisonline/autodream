dc9b884fdb7b2201efcb0e742d0360a4fd8fc544
Carter Reeb <carter2k4@pm.me>
2021-05-19 16:55:53 -0500

    Switched to Apache log4j

--- src/main/java/com/carterisonline/autodream/mixin/ExampleMixin.java
+++ src/main/java/com/carterisonline/autodream/mixin/ExampleMixin.java
@@ -5,12 +5,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.apache.logging.log4j.Logger;
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 @Mixin(TitleScreen.class)
 // Mixins HAVE to be written in java due to constraints in the mixin system.
 public class ExampleMixin {
+    private static final Logger log = getLogger(ExampleMixin.class.getName());
+
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        System.out.println("This line is printed by an example mod mixin!");
+        log.info("This line is printed by an example mod mixin!");
     }
 }
